566d39b7f03e34f014809cd9c788f30df4ff0bcf
ryuchun00 <leokun0210@gmail.com>
2019-12-01 15:22:28 +0900

    色をランダムで変更することができる

--- 6/packman.rb
+++ 6/packman.rb
@@ -179,8 +179,6 @@ def mypicture4
 end
 
 # --- ここからパックマンの敵、ゴーストを作るためのメソッド集
-#
-#
 
 # 凸型に塗りつぶすメソッド
 # 正方形を4つ並べる
@@ -197,11 +195,11 @@ def fill_convex(x, y, h, r = 0, g = 0, b = 0, a = 0.0)
   fillrect(x + h, y, h, h, r, g, b, a)
 end
 
-def two_convex(y, w, r = 0, g = 0, b = 0, a = 0.0)
+def two_convex(x, y, w, r = 0, g = 0, b = 0, a = 0.0)
   # 足の太さは4px
   2.times do |i|
-    # 0,1
-    fill_convex(w / 3 / 2 + w / 3 * 2 * i, y, 4, r, g, b, a)
+    # wを二等分して、その中の1/3の部分にxを打つ
+    fill_convex(x - w / 3 + w / 3 * 2 * i, y, 4, r, g, b, a)
   end
 end
 
@@ -209,15 +207,23 @@ end
 def ghosts(n = 1)
   w = 60 # 胴体部分に当たる長方形の横の長さ
   h = 30 # 胴体部分に当たる長方形の縦の長さ
-  x = 30 # 胴体部分に当たる長方形のx軸
-  y = 40 # 胴体部分に当たる長方形のy軸
+  x = rand((w / 2)..(300 - w / 2)) # 胴体部分に当たる長方形のx軸
+  y = rand((10 + h / 2)..(200 - h / 2)) # 胴体部分に当たる長方形のy軸
   s = 4 # ゴーストの両足に当たる凸を構成する正方形4つのうちの1辺の長さ
+
+  # 赤、ピンク、水色、オレンジ
+  red = [255, 0, 0] # 赤
+  pink = [255, 182, 193] # ピンク
+  light_blue = [0, 255, 255]
+  orange = [255, 165, 0]
+  color = [red, pink, light_blue, orange].sample
+
   # まずゴーストの胴体である長方形を配置する
-  fillrect(x, y, w, h, 0, 255, 255, 0.0)
+  fillrect(x, y, w, h, color[0], color[1], color[2], 0.0)
   # ゴーストの頭となるような楕円を配置する
-  fillellipse(x, 25, w / 2, 10, 0, 255, 255, 0.0)
+  fillellipse(x, y - h / 2, w / 2, 10, color[0], color[1], color[2], 0.0)
   # 足を切り抜く
-  two_convex(y + h / 2 - s / 2, w, 255, 255, 255, 0.0)
+  two_convex(x, y + h / 2 - s / 2, w, 255, 255, 255, 0.0)
   fillrect(x, y + h / 2 - s, s * 2, s * 2, 255, 255, 255, 0.0)
   # ゴーストの目となるような楕円を配置する
   rx = 6
