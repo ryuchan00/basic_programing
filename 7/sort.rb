7267a301160623a6409e865d9060f75208ae714e
ryuchun00 <leokun0210@gmail.com>
2019-12-02 20:20:54 +0900

    演習1

---
+++ 7/sort.rb
@@ -0,0 +1,70 @@
+def min(a)
+  min = a[0]
+  a.each_index do |i|
+    if min > a[i] then
+      min = a[i]
+    end
+  end
+  return min
+end
+
+def minpos(a)
+  min = a[0]; pos = 0
+  a.each_index do |i|
+    if min > a[i] then
+      min = a[i]
+      pos = i
+    end
+  end
+  return pos
+end
+
+def swap(a, i, j)
+  x = a[i]
+  a[i] = a[j]
+  a[j] = x
+end
+
+def bubblesort(a)
+  done = false
+  while !done do
+    done = true
+    0.step(a.length - 2) do |i|
+      if a[i] > a[i + 1] then
+        swap(a, i, i + 1); done = false
+      end
+    end
+  end
+end
+
+a = [1, 9, 5, 4, 2]
+bubblesort(a)
+p a
+
+def randarray(n)
+  return Array.new(n) do
+    rand(10000)
+  end
+end
+
+p randarray 10
+
+def arrayminrange(a, i, j)
+  min = a[i]
+  max = a[i]
+  (i..j).each do |k|
+    min = a[k] if a[i] > a[k]
+    max = a[k] if a[i] < a[k]
+  end
+  return [min, max]
+end
+
+def selectionsort(a)
+  a.each_with_index do |v, i|
+
+  end
+end
+
+p arrayminrange([100, 2, 5, 10], 1, 3)
+
+
