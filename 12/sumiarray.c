5bae2a89aa1bce50d19eb591030f18dea5f3ba5b
ryuchun00 <leokun0210@gmail.com>
2020-01-20 20:33:24 +0900

    sumiarrayはintで返すことが指定されている

--- 12/sumiarray.c
+++ 12/sumiarray.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-void sumiarray(int n, int a[]) {
+int sumiarray(int n, int a[]) {
     int i, sum;
     sum = 0;
     for (i = 0; i < n; ++i) {
 //        printf(" %d\n", a[i]);
         sum += a[i];
     }
-    printf("%d\n", sum);
+    return sum;
 }
 
 int main(void) {
     int a[24] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-    sumiarray(24, a);
+    printf("%d\n", sumiarray(24, a));
     return 0;
 }
