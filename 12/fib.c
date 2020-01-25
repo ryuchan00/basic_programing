c6d21dc14085da4070cb4233f31684240b77b4ec
ryuchun00 <leokun0210@gmail.com>
2020-01-25 21:28:10 +0900

    演習3 実際に上に説明した方法で「0番目から30番目までのフィボナッチ数列を 打ち出す」プログラムを作りなさい。さらに、「0～30 までのいずれかの番号 を入力すると、その番号のフィボナッチ数列を出力する」プログラムも作れる となおよい。

---
+++ 12/fib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, n, fib[31] = {1, 1};
+    printf("Fib Array");
+    printf("\n");
+    for (i = 2; i <= 30; ++i) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+        printf("%d: %2d", i, fib[i]);
+        printf("\n");
+    }
+    printf("n> ");
+    scanf("%d", &n);
+    printf(" %2d", fib[n]);
+    printf("\n");
+    return 0;
+}
