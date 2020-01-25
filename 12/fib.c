29f076ac529443921fd95f5ee09299a536916888
ryuchun00 <leokun0210@gmail.com>
2020-01-25 21:32:24 +0900

    0~30までのフィボナッチ数列を出す

--- 12/fib.c
+++ 12/fib.c
@@ -4,6 +4,10 @@ int main(void) {
     int i, n, fib[31] = {1, 1};
     printf("Fib Array");
     printf("\n");
+    printf("%d: %2d", 0, fib[0]);
+    printf("\n");
+    printf("%d: %2d", 1, fib[1]);
+    printf("\n");
     for (i = 2; i <= 30; ++i) {
         fib[i] = fib[i - 1] + fib[i - 2];
         printf("%d: %2d", i, fib[i]);
